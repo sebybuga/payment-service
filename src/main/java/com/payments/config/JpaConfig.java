afdb75eb5665a23c548f0b93816601081d253f2f
sebybuga <seby_buga@yahoo.com>
2026-04-22 20:20:15 +0300

    Fix: ModelMapper bean moved in a dedicated class.

--- src/main/java/com/payments/config/JpaConfig.java
+++ src/main/java/com/payments/config/JpaConfig.java
@@ -1,8 +1,7 @@
 package com.payments.config;
 
-import org.modelmapper.ModelMapper;
+
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
@@ -12,10 +11,4 @@ import javax.sql.DataSource;
 @ConditionalOnBean(DataSource.class)
 @EnableJpaAuditing
 public class JpaConfig {
-
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
-
 }
